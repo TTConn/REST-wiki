97f91b0d575b8f4d6c79dc4898fe49f36929e0de
Tobias Conn <tobias.conn@gmail.com>
2022-03-15 21:15:21 -0400

    initial commit, completed REST API for wiki project

---
+++ app.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+    useNewUrlParser: true
+});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+// Requests for ALL articles ---------------------------------------------------
+// route for chain methods: get, post, delete
+app.route("/articles")
+
+    .get((req, res) => {
+        Article.find((err, foundArticles) => {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("Success: new article added.")
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .delete((req, res) => {
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Success: all articles delted.");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+// Requests for SPECIFIC articles ----------------------------------------------
+
+app.route("/articles/:articleTitle")
+
+    .get((req, res) => {
+        Article.findOne(
+            {title: req.params.articleTitle},
+            (err, foundArticle) => {
+                if (foundArticle) {
+                    res.send(foundArticle);
+                } else {
+                    res.send("No articles found with that title.")
+                }
+            }
+        );
+    })
+
+    .put((req, res) => {
+        Article.updateOne(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+            {upsert: true},
+            (err) => {
+                if (!err) {
+                    res.send("Success: article updated.");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    })
+
+    .patch((req, res) => {
+        Article.updateOne(
+            {title: req.params.articleTitle},
+            {$set: req.body},
+            (err) => {
+                if (!err) {
+                    res.send("Success: article updated/patched.");
+                } else {
+                    res.send(err);
+                }
+            }
+        )
+    })
+
+    .delete((req, res) => {
+        Article.deleteOne(
+            {title: req.params.articleTitle},
+            (err) => {
+                if (!err) {
+                    res.send("Success: article deleted.");
+                } else {
+                    res.send(err);
+                }
+            }
+        )
+    });
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
